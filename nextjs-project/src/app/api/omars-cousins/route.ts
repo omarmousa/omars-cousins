04c55883a859a5a67ab96f652402c1396df25f3f
omarmousa <omousa16@gmail.com>
2025-07-16 09:33:24 -0500

    changing model

--- nextjs-project/src/app/api/omars-cousins/route.ts
+++ nextjs-project/src/app/api/omars-cousins/route.ts
@@ -10,7 +10,7 @@ export const POST = async (request: NextRequest) => {
       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
     },
     body: JSON.stringify({
-      model: 'gpt-4o',
+      model: 'gpt-3.5-turbo',
       messages: [
         {
           role: 'user',
